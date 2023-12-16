cc53a0996d27d1d5ac2f4fef0aa67c46196b8259
Ayberk Kahraman <ayberkkahraman99@gmail.com>
2023-12-16 17:40:55 +0300

    #update

---
+++ Assets/Project/_Scripts/Runtime/InGame/PerlinCube/PerlinCube.cs
@@ -0,0 +1,54 @@
+﻿using DG.Tweening;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Project._Scripts.Runtime.InGame.PerlinCube
+{
+  public class PerlinCube : MonoBehaviour
+  {
+    #region Components
+    private MeshRenderer _meshRenderer;
+    #endregion
+    private float _perlinScale = 1f;
+    
+
+    private float _currentColorValue;
+    private float _targetHeight;
+
+    private Vector3 _defaultScale;
+    private Vector3 _currentScale;
+
+    private bool _ready;
+
+    private void Start()
+    {
+      Initialize();
+    }
+    
+    public void CubeUpdate(float speed, Vector2 limits)
+    {
+      _targetHeight = HeightCalculation(speed, limits);
+      _currentColorValue = Mathf.Lerp(0.2f, 1f, (_targetHeight - limits.x) / (limits.y - limits.x));
+
+      _currentScale = transform.localScale;
+      _currentScale.y = _targetHeight;
+      transform.localScale = _currentScale;
+
+      _meshRenderer.material.color = new Color(_currentColorValue, _currentColorValue, _currentColorValue);
+    }
+
+    private float HeightCalculation(float speed, Vector2 limits)
+    {
+      float perlinValue = Mathf.PerlinNoise(Time.time * _perlinScale * speed, 0.0f);
+      float height = Mathf.Lerp(limits.x, limits.y, perlinValue);
+      return height;
+    }
+
+    private void Initialize()
+    {
+      _meshRenderer = GetComponent<MeshRenderer>();
+      _perlinScale = Random.Range(.01f, 1f);
+    }
+  }
+}
---
+++ Assets/Project/_Scripts/Runtime/InGame/TileGenerator/PerlinTileGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Project._Scripts.Runtime.InGame.TileGenerator
+{
+  public class PerlinTileGenerator : MonoBehaviour
+  {
+    public PerlinCube.PerlinCube PerlinCube;
+    public Transform CubesHolder;
+    public Vector2Int Size;
+    public Vector2 Offset;
+
+    public AnimationCurve InitializeEase;
+
+    private List<PerlinCube.PerlinCube> _cubes;
+    
+    [Range(.01f, 3f)][SerializeField] private float NoiseSpeed = 1f;
+    
+    [MinMaxSlider(.2f, 1f)]
+    public Vector2 HeightBoundaries;
+
+    private void Start()
+    {
+      _cubes = new List<PerlinCube.PerlinCube>();
+      
+      GenerateTile();
+    }
+    private void Update()
+    {
+      if(Input.GetKeyDown(KeyCode.K)) GenerateTile();
+
+      if (_cubes.Any())
+      {
+        _cubes.ForEach(x => x.CubeUpdate(NoiseSpeed, HeightBoundaries));
+      }
+    }
+    
+    
+
+    public void GenerateTile()
+    {
+      _cubes.ForEach(x => Destroy(x.gameObject));
+      _cubes = new List<PerlinCube.PerlinCube>();
+
+      for (int x = 0; x < Size.x; x++)
+      {
+        for (int y = 0; y < Size.y; y++)
+        {
+          Vector2 position = new Vector2
+          (
+            x + (PerlinCube.transform.localScale.x / 2) + Offset.x*x - (Size.x / 2), 
+            y + (PerlinCube.transform.localScale.z / 2) + Offset.y*y - (Size.y / 2)
+          );
+          
+          Vector3 initialPosition = new Vector3(position.x, 0f, position.y);
+          PerlinCube.PerlinCube cube = Instantiate(PerlinCube, initialPosition, Quaternion.identity, CubesHolder);
+          _cubes.Add(cube);
+        }
+      }
+    }
+  }
+}
